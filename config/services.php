0af482e5c40e6e5c54c67cd9abcd65047a631df2
bhavs123 <bhavana@infiniteit.biz>
2015-12-08 17:31:47 +0530

    service

--- config/services.php
+++ config/services.php
@@ -17,7 +17,7 @@ return [
     'mailgun' => [
         'domain' => env('MAILGUN_DOMAIN'),
         'secret' => env('MAILGUN_SECRET'),
-    ],
+    ], 
 
     'mandrill' => [
         'secret' => env('vo3pKWCdNFdqHOZ7EWoXVg'),
